2b9a010626f0e3472efce7cc70d56af0380defc0
moundu <munenemuondu@gmail.com>
2020-08-02 16:14:11 +0300

    I have added sqlite3

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
-def logic():
-    a = input("Enter one name:  ")
-    print("You entered: " + a)
+import sqlite3
+conn = sqlite3.connect('main.db')
+c = conn.cursor()
 
-    with open('main.txt','a') as f:
-        f.write(a)
-        f.write("\n")
-        def input_algorithim():
-            continu_e = input("Do you want to enter another name? Kindly choose Yes(Y) or no(N)? ")
-            if continu_e == "Y" or continu_e == "y":
-                logic()
-            elif continu_e == "N" or continu_e == "n":
-                print("Thank you.")
-                f.close()
-            else:
-                print("You did not chose an option above!")
-                input_algorithim()
-        input_algorithim()
-
-logic()
+def create_table():
+    c.execute('CREATE TABLE IF NOT EXISTS name(name TEXT)')
+create_table()
+def updating_table():
+    while True:
+        name1 = input("Enter one name:  ")
+                    
+        print("You entered: " + name1)
+        continu_e = input("Do you want to enter another name? Kindly choose Yes(Y) or no(N)? ")
+        if continu_e == "Y" or continu_e == "y":
+            continue
+        elif continu_e == "N" or continu_e == "n":
+            print("Thank you.")
+            print("You did not chose an option above!")
+            c.execute("INSERT INTO name(name) VALUES(?)",(name1,))
+            conn.commit()
+            print("Prossed nicely")
+            break
+updating_table()
+def reading_table():
+    c.execute('SELECT * FROM name')
+    data = c.fetchall()
+    for kol in data:
+        print(kol)
+reading_table()
